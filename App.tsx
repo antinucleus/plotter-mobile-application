5c13c3c35cca233107b763de3e62173674758716
antinucleus <antinucleuss@gmail.com>
2024-05-22 16:39:11 +0300

    feat: add axios, react-natvigation, vector-icons and expo-network feat(lib): add axios

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Text>Plotter Controller</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider>
+        <SafeAreaView style={styles.container}>
+          <Text>Plotter Controller</Text>
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
