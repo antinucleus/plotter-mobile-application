df890e0899eda421aab4bb662280828910310cd6
antinucleus <antinucleuss@gmail.com>
2024-06-04 12:42:51 +0300

    refactor(config): remove unused urls

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'http://192.168.159.107:3000'; // raspberry
-// export const API_URL = 'http://192.168.159.153:3000'; // local
+export const API_URL = 'http://10.42.0.1:3000'; // raspberry
+
 export const STATIC_MEDIA_URL = `${API_URL}/public/result.svg`;
 export const MACHINE_AXIS_X_LIMIT = 145;
 export const MACHINE_AXIS_Y_LIMIT = 210;
